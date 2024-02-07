a0b50609a5c2b40ccb673dcb9b1e9f33d041ceb8
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-02-07 10:04:49 +0200

    update Button component props

--- src/02-components/08-using-html-props.problem.tsx
+++ src/02-components/08-using-html-props.problem.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+  className?: string;
+}
+
 export const Button = ({ className, ...rest }: {}) => {
+  // export const Button = (props: ButtonProps) => {
+  // const { className, ...rest } = props;
   return (
     <button {...rest} className={`default-classname ${className}`}></button>
   );
